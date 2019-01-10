095cedb2149bde9437eb9571871242645bee46de
Rene Donner <code@donner.at>
2019-01-10 07:54:16 +0100

    only use hardcoded filenames in deployed mode

--- src/MbedTLS.jl
+++ src/MbedTLS.jl
@@ -38,11 +38,17 @@ export
 import Base: show
 
 # Load mbedtls libraries from our deps.jl
-const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("MbedTLS not installed properly, run Pkg.build(\"MbedTLS\"), restart Julia and try again")
+if "DEV" in ARGS
+    const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
+    if !isfile(depsjl_path)
+        error("MbedTLS not installed properly, run Pkg.build(\"MbedTLS\"), restart Julia and try again")
+    end
+    include(depsjl_path)
+else
+    libmbedtls = "libmbedtls.so"
+    libmbedcrypto = "libmbedcrypto.so"
+    libmbedx509 = "libmbedx509.so"
 end
-include(depsjl_path)
 
 const MBED_SUCCESS = Cint(0)
 
